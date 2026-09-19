cbda00f428b7dd67d805de2c4eab92661e4ee679
Woong Jun <woong.jun@gmail.com>
2026-09-19 21:47:45 +0900

    style(client/ios): desaturate widget cover art in tinted glass mode

--- client/ios/CanaryWidget/CanaryWidget.swift
+++ client/ios/CanaryWidget/CanaryWidget.swift
@@ -225,7 +225,7 @@ struct PlayerWidgetView: View {
         if let data = entry.coverData, let uiImage = UIImage(data: data) {
             Image(uiImage: uiImage)
                 .resizable()
-                .widgetAccentedRenderingMode(.fullColor)
+                .widgetAccentedRenderingMode(.accentedDesaturated)
                 .aspectRatio(contentMode: .fill)
         } else {
             Color.gray.opacity(0.2)
